15ae824664de0a00b078ab206414dfabcc3ce222
muji <freeformsystems@gmail.com>
2016-03-14 23:31:39 +0800

    Assert on @source directive.

---
+++ test/spec/source.js
@@ -0,0 +1,46 @@
+var fs = require('fs')
+  , expect = require('chai').expect
+  , mkpi = require('../../index')
+  , mkast = require('mkast')
+  , Parser = mkast.Parser;
+
+describe('mkpi:', function() {
+
+  it('should process @source pi', function(done) {
+    var source = 'test/fixtures/source.md'
+      , target = 'target/source.json.log'
+      , parser = new Parser()
+      , data = parser.parse('' + fs.readFileSync(source))
+      , instructions = [
+          '<?\n  @source {javascript} source.js\n?>'
+        ]
+
+    // mock file for correct relative path
+    // mkcat normally injects this info
+    data._file = source;
+
+    var input = mkast.serialize(data)
+      , output = fs.createWriteStream(target)
+      , opts = {input: input, output: output};
+    
+    mkpi(opts);
+
+    output.once('finish', function(err) {
+      var result = ('' + fs.readFileSync(target)).trim().split('\n');
+      result = result.map(function(line) {
+        return JSON.parse(line); 
+      })
+
+      expect(result).to.be.an('array');
+      expect(result[1]._literal).to.eql(instructions[0]);
+
+      // skip the open document
+
+      expect(result[3]._literal)
+        .to.eql('module.exports = function source(){};\n');
+
+      done(err);
+    })
+  });
+
+});
