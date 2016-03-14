7f66e5afbd27ebe369a821b08ff25218cc32fed7
muji <freeformsystems@gmail.com>
2016-03-14 21:36:02 +0800

    Improve test spec.

--- test/spec/mkpi.js
+++ test/spec/mkpi.js
@@ -11,6 +11,10 @@ describe('mkpi:', function() {
       , target = 'target/include.json.log'
       , parser = new Parser()
       , data = parser.parse('' + fs.readFileSync(source))
+      , instructions = [
+          '<? \n  @include inc.md \n  @unknown\n?>',
+          '<? @include deep.md ?>'
+        ]
 
     // mock file for correct relative path
     // mkcat normally injects this info
@@ -29,12 +33,27 @@ describe('mkpi:', function() {
       })
 
       expect(result).to.be.an('array');
-      console.dir(result);
       expect(result[0]._type).to.eql('document');
       expect(result[1]._type).to.eql('paragraph');
       expect(result[1]._firstChild._literal).to.eql('Paragraph before.');
-      //expect(result[1]._type).to.eql('paragraph');
-      //
+      expect(result[2]._literal).to.eql(instructions[0]);
+
+      // skip the open document
+
+      expect(result[4]._level).to.eql(2);
+      expect(result[4]._firstChild._literal).to.eql('Include');
+
+      // skip the include paragraph
+
+      expect(result[6]._literal).to.eql(instructions[1]);
+
+      // skip the open document
+
+      expect(result[8]._firstChild._literal)
+        .to.eql('This is a paragraph in a deep include.');
+
+      expect(result[result.length - 2]._firstChild._literal)
+        .to.eql('Paragraph after.');
       expect(result[result.length - 1]._type).to.eql('eof');
       done(err);
     })
