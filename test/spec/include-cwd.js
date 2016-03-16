812c2e86d2f91e469fbd9e969f6a3497983bfe3f
muji <freeformsystems@gmail.com>
2016-03-16 14:32:09 +0800

    Update @include test specs.

---
+++ test/spec/include-cwd.js
@@ -0,0 +1,47 @@
+var fs = require('fs')
+  , expect = require('chai').expect
+  , mkpi = require('../../index')
+  , mkast = require('mkast')
+  , Parser = mkast.Parser;
+
+describe('mkpi:', function() {
+
+  it('should process @include pi', function(done) {
+    var source = 'test/fixtures/include-cwd.md'
+      , target = 'target/include-cwd.json.log'
+      , parser = new Parser()
+      , data = parser.parse('' + fs.readFileSync(source))
+      , instructions = [
+          '<? @include .gitignore ?>',
+        ]
+
+    // NOTE: do not inject _file to change path resolution
+
+    var input = mkast.serialize(data)
+      , output = fs.createWriteStream(target)
+      , opts = {input: input, output: output, preserve: true};
+    
+    mkpi(opts);
+
+    output.once('finish', function(err) {
+      var result = ('' + fs.readFileSync(target)).trim().split('\n');
+      result = result.map(function(line) {
+        return JSON.parse(line); 
+      })
+
+      expect(result).to.be.an('array');
+
+      // skip the open document
+
+      expect(result[1]._literal).to.eql(instructions[0]);
+
+      // skip the open document
+
+      expect(result[3]._type).to.eql('paragraph');
+
+      expect(result[result.length - 1]._type).to.eql('eof');
+      done(err);
+    })
+  });
+
+});
---
+++ test/spec/include-read-error.js
@@ -0,0 +1,32 @@
+var fs = require('fs')
+  , expect = require('chai').expect
+  , mkpi = require('../../index')
+  , mkast = require('mkast')
+  , Parser = mkast.Parser;
+
+describe('mkpi:', function() {
+
+  it('should process @include pi (file read error)', function(done) {
+    var include = 'test/fixtures/include-read-error.md'
+      , target = 'target/include-read-error.json.log'
+      , parser = new Parser()
+      , data = parser.parse('' + fs.readFileSync(include));
+
+    // mock file for correct relative path
+    // mkcat normally injects this info
+    data._file = include;
+
+    var input = mkast.serialize(data)
+      , output = fs.createWriteStream(target)
+      , opts = {input: input, output: output, preserve: true};
+    
+    mkpi(opts, function(err) {
+      function fn() {
+        throw err; 
+      }
+      expect(fn).throws(/ENOENT/i);
+      done();
+    });
+  });
+
+});
